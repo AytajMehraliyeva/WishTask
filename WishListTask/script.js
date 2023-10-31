e2939ea5c8f3c818e610c2f6b7af9c4e42aa588b
AytajMehraliyeva <tu7lzxxdc@code.edu.az>
2023-10-31 10:53:34 +0400

    Wishlist task

---
+++ WishListTask/script.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const openNav = document.getElementById("openNavBtn")
+    const addBtnProduct = document.getElementById("addBtn");
+    const productNameInput = document.getElementById("productName");
+    const productCategoryInput = document.getElementById("productCategory");
+    const productBrandInput = document.getElementById("productBrand");
+    const productPriceInput = document.getElementById("productPrice");
+    const productContainer = document.getElementById("productContainer");
+    const wishlistContainer = new bootstrap.Offcanvas(document.getElementById("wishList"));
+
+    class Product {
+        constructor(name, category, brand, price) {
+            this.name = name;
+            this.category = category;
+            this.brand = brand;
+            this.price = price;
+        }
+    }
+
+    openNav.addEventListener("click", () => {
+        wishlistContainer.show();
+
+    });
+
+    addBtnProduct.addEventListener("click", () => {
+        const name = productNameInput.value;
+        const category = productCategoryInput.value;
+        const brand = productBrandInput.value;
+        const price = productPriceInput.value;
+
+        const newProduct = new Product(name, category, brand, price);
+        displayProduct(newProduct);
+
+        const products = JSON.parse(localStorage.getItem("products")) || [];
+        products.push(newProduct);
+        localStorage.setItem("products", JSON.stringify(products));
+    });
+
+    function displayProduct(product) {
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("product_card");
+        cardDiv.innerHTML = `
+            <h3>${product.name}</h3>
+            <p>Category: ${product.category}</p>
+            <p>Brand: ${product.brand}</p>
+            <p>Price: $${product.price}</p>
+            <button class="wishlist-btn">Add to Wishlist</button>
+        `;
+
+        const wishlistBtn = cardDiv.querySelector(".wishlist-btn");
+        wishlistBtn.addEventListener("click", function() {
+            addToWishlist(product);
+        });
+
+        productContainer.appendChild(cardDiv);
+    }
+
+    function addToWishlist(product) {
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("product_card");
+        cardDiv.innerHTML = `
+            <h3>${product.name}</h3>
+            <p>Category: ${product.category}</p>
+            <p>Brand: ${product.brand}</p>
+            <p>Price: $${product.price}</p>
+        `;
+        wishlistContainer.querySelector(".offcanvas-body").appendChild(cardDiv);
+
+        const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+        wishlist.push(product);
+        localStorage.setItem("wishlist", JSON.stringify(wishlist));
+    }
+
+
+    const products = JSON.parse(localStorage.getItem("products")) || [];
+    products.forEach(product => {
+        displayProduct(product);
+    });
+
+    const wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+    wishlist.forEach(product => {
+        addToWishlist(product);
+    });
+});
